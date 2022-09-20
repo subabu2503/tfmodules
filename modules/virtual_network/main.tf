04391a179739e67280985814a39f73231f785707
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2022-09-20 15:42:19 -0500

    Update main.tf

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -13,7 +13,8 @@ resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
     resource_group_name  = "${var.resource_group_name}"
     virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
-    address_prefixes       = "10.0.1.0/24"
+    address_prefixes       = ["10.0.1.0/24"]
+    
 }
 
 # Create public IPs
@@ -21,7 +22,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
     location                     = "${var.location}"
     resource_group_name          = "${var.resource_group_name}"
-    allocation_method   = "dynamic"
+    allocation_method   = "Dynamic"
 
 }
 
